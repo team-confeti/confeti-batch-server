7c1a19ac23dfb9b66f97ae993326db2dfc3a5155
jher235 <tim668666@gmail.com>
2025-11-23 21:09:05 +0900

    CONFETI-57 init: 프로젝트 초기 세팅

---
+++ src/test/java/confeti/confetibatchserver/ConfetiBatchServerApplicationTests.java
@@ -0,0 +1,13 @@
+package confeti.confetibatchserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfetiBatchServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
