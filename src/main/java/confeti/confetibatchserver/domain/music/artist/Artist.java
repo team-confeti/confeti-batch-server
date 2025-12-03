12b33ff50afee43b76ba1dc3ac9bbc95adcfdb3d
jher235 <tim668666@gmail.com>
2025-12-03 23:49:03 +0900

    CONFETI-12 feat: Artist 엔티티 작성 - 목적이 배치 처리이므로 연관관계는 작성하지 않음

---
+++ src/main/java/confeti/confetibatchserver/domain/music/artist/Artist.java
@@ -0,0 +1,40 @@
+package confeti.confetibatchserver.domain.music.artist;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "artists")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Artist {
+
+    @Id
+    @Column(nullable = false)
+    private String id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(length = 3000)
+    private String artworkUrl;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
