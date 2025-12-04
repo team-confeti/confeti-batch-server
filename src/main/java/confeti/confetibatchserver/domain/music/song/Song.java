e1005efb5def6a123e8f9abe9c9232411e85123d
jher235 <tim668666@gmail.com>
2025-12-04 13:34:12 +0900

    CONFETI-12 feat: Song 엔티티 작성

---
+++ src/main/java/confeti/confetibatchserver/domain/music/song/Song.java
@@ -0,0 +1,46 @@
+package confeti.confetibatchserver.domain.music.song;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "songs")
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Song {
+
+    @Id
+    @Column(nullable = false)
+    private String id;
+
+    @Column(length = 100, nullable = false)
+    private String trackName;
+
+    @Column(length = 100)
+    private String artistName;
+ 
+    @Column(length = 3000)
+    private String artworkUrl;
+
+    @Column(length = 3000)
+    private String previewUrl;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
+
