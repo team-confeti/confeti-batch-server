2409f972db25f7ce1a8b57ff17f60d60d1d96679
jher235 <tim668666@gmail.com>
2025-12-24 18:57:21 +0900

    CONFETI-75 feat: artistId 기반으로 Song을 찾는 메서드 추가

--- src/main/java/confeti/confetibatchserver/domain/music/artistsong/infra/repository/ArtistSongRepository.java
+++ src/main/java/confeti/confetibatchserver/domain/music/artistsong/infra/repository/ArtistSongRepository.java
@@ -1,8 +1,19 @@
 package confeti.confetibatchserver.domain.music.artistsong.infra.repository;
 
 import confeti.confetibatchserver.domain.music.artistsong.ArtistSong;
+import confeti.confetibatchserver.domain.music.song.Song;
+import feign.Param;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 
 public interface ArtistSongRepository extends JpaRepository<ArtistSong, Long> {
 
+    @Query(value = """
+        SELECT s
+        FROM Song as s
+        JOIN ArtistSong AS a_s ON a_s.artistId = :artistId
+        """)
+    List<Song> findAllSongByArtistId(@Param("artistId") String artistId);
 }
