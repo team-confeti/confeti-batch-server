8efb133270105b60e31dd5f4e74e50e32f5a640f
jher235 <tim668666@gmail.com>
2025-12-24 18:49:54 +0900

    CONFETI-75 feat: SongService 구현

---
+++ src/main/java/confeti/confetibatchserver/domain/music/song/application/SongService.java
@@ -0,0 +1,31 @@
+package confeti.confetibatchserver.domain.music.song.application;
+
+import confeti.confetibatchserver.domain.music.artistsong.infra.repository.ArtistSongRepository;
+import confeti.confetibatchserver.domain.music.song.Song;
+import confeti.confetibatchserver.domain.music.song.infra.repository.SongRepository;
+import confeti.confetibatchserver.domain.music.song.vo.ConfetiSong;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SongService {
+
+    private final SongRepository songRepository;
+    private final ArtistSongRepository artistSongRepository;
+
+    public Map<String, Song> getSongMapByArtistId(String artistId) {
+        return artistSongRepository.findAllSongByArtistId(artistId).stream()
+            .collect(Collectors.toMap(Song::getId, Function.identity()));
+    }
+
+    public void upsertSongsWithArtistId(String artistId, List<ConfetiSong> songs) {
+        songRepository.upsertSongsWithArtistId(artistId, songs);
+    }
+}
