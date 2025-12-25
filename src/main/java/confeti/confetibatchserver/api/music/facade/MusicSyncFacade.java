4a5b3b6759454887b04115ce894ad75e8619f3a5
jher235 <tim668666@gmail.com>
2025-12-26 01:55:25 +0900

    CONFETI-75 refactor: upsert 로직을 SongService 내부에 두도록 변경

--- src/main/java/confeti/confetibatchserver/api/music/facade/MusicSyncFacade.java
+++ src/main/java/confeti/confetibatchserver/api/music/facade/MusicSyncFacade.java
@@ -4,13 +4,10 @@ import confeti.confetibatchserver.domain.music.artist.Artist;
 import confeti.confetibatchserver.domain.music.artist.application.ArtistService;
 import confeti.confetibatchserver.domain.music.artist.vo.ConfetiArtist;
 import confeti.confetibatchserver.domain.music.song.application.SongService;
-import confeti.confetibatchserver.domain.music.song.projection.SongProjection;
 import confeti.confetibatchserver.domain.music.song.vo.ConfetiSong;
 import confeti.confetibatchserver.external.service.MusicAPIHandler;
 import confeti.confetibatchserver.global.annotation.Facade;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -25,29 +22,7 @@ public class MusicSyncFacade {
 
     public void upsertSongByArtistId(String artistId) {
         List<ConfetiSong> fetchedSongs = musicAPIHandler.getAllSongsByArtistId(artistId);
-        Map<String, SongProjection> songMapByArtistId = songService.getConfetiSongMapByArtistId(
-            artistId);
-        List<ConfetiSong> upsertSongs = getUpsertSongs(fetchedSongs, songMapByArtistId);
-
-        songService.upsertSongsWithArtistId(artistId, upsertSongs);
-    }
-
-    private List<ConfetiSong> getUpsertSongs(
-        List<ConfetiSong> fetchedSongs,
-        Map<String, SongProjection> songMapByArtistId
-    ) {
-        List<ConfetiSong> upsertSongs = new ArrayList<>();
-        for (ConfetiSong fetchedSong : fetchedSongs) {
-            SongProjection song = songMapByArtistId.get(fetchedSong.getId());
-            if (song == null) {
-                upsertSongs.add(fetchedSong);
-                continue;
-            }
-            if (fetchedSong.isDifferentData(song)) {
-                upsertSongs.add(fetchedSong);
-            }
-        }
-        return upsertSongs;
+        songService.upsert(artistId, fetchedSongs);
     }
 
     public void upsertArtists(List<ConfetiArtist> artists) {
--- src/main/java/confeti/confetibatchserver/domain/music/song/application/SongService.java
+++ src/main/java/confeti/confetibatchserver/domain/music/song/application/SongService.java
@@ -1,9 +1,9 @@
 package confeti.confetibatchserver.domain.music.song.application;
 
-import confeti.confetibatchserver.domain.music.artistsong.infra.repository.ArtistSongRepository;
 import confeti.confetibatchserver.domain.music.song.infra.repository.SongRepository;
 import confeti.confetibatchserver.domain.music.song.projection.SongProjection;
 import confeti.confetibatchserver.domain.music.song.vo.ConfetiSong;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -18,14 +19,36 @@ import org.springframework.stereotype.Service;
 public class SongService {
 
     private final SongRepository songRepository;
-    private final ArtistSongRepository artistSongRepository;
 
-    public Map<String, SongProjection> getConfetiSongMapByArtistId(String artistId) {
+    @Transactional
+    public void upsert(String artistId, List<ConfetiSong> songs) {
+        Map<String, SongProjection> songMapByArtistId = getConfetiSongMapByArtistId(
+            artistId);
+        List<ConfetiSong> upsertSongs = getUpsertSongs(songs, songMapByArtistId);
+
+        songRepository.upsertSongsWithArtistId(artistId, upsertSongs);
+    }
+
+    private Map<String, SongProjection> getConfetiSongMapByArtistId(String artistId) {
         return songRepository.findAllSongByArtistId(artistId).stream()
             .collect(Collectors.toMap(SongProjection::getId, Function.identity()));
     }
 
-    public void upsertSongsWithArtistId(String artistId, List<ConfetiSong> songs) {
-        songRepository.upsertSongsWithArtistId(artistId, songs);
+    private List<ConfetiSong> getUpsertSongs(
+        List<ConfetiSong> fetchedSongs,
+        Map<String, SongProjection> songMapByArtistId
+    ) {
+        List<ConfetiSong> upsertSongs = new ArrayList<>();
+        for (ConfetiSong fetchedSong : fetchedSongs) {
+            SongProjection song = songMapByArtistId.get(fetchedSong.getId());
+            if (song == null) {
+                upsertSongs.add(fetchedSong);
+                continue;
+            }
+            if (fetchedSong.isDifferentData(song)) {
+                upsertSongs.add(fetchedSong);
+            }
+        }
+        return upsertSongs;
     }
 }
