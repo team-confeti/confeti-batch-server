e81fad1464991d7c9c51212e2700d0bce560760c
jher235 <tim668666@gmail.com>
2025-12-26 00:41:49 +0900

    CONFETI-75 feat: id 기반으로 Artist 호출하는 로직을 MusicAPIHandler 가 갖도록 변경

--- src/main/java/confeti/confetibatchserver/external/service/AppleMusicAPIHandler.java
+++ src/main/java/confeti/confetibatchserver/external/service/AppleMusicAPIHandler.java
@@ -1,10 +1,13 @@
 package confeti.confetibatchserver.external.service;
 
+import confeti.confetibatchserver.domain.music.artist.vo.ConfetiArtist;
 import confeti.confetibatchserver.domain.music.song.vo.ConfetiSong;
 import confeti.confetibatchserver.external.client.AppleMusicFeignClient;
+import confeti.confetibatchserver.external.client.dto.artist.AppleMusicArtistsResponse;
 import confeti.confetibatchserver.external.client.dto.music.AppleMusicMusicResponse;
 import confeti.confetibatchserver.external.client.dto.music.AppleMusicMusicsResponse;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,7 +16,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AppleMusicAPIHandler implements MusicAPIHandler {
 
+    private final static String QUERY_PARAMETER_IDS_DELIMITER = ",";
     private final static int ARTIST_TOP_SONG_FETCH_SIZE = 100;
+
     private final AppleMusicFeignClient appleMusicFeignClient;
 
     @Override
@@ -32,5 +37,13 @@ public class AppleMusicAPIHandler implements MusicAPIHandler {
         } while (next != null);
 
         return allSongs.stream().map(AppleMusicMusicResponse::toConfetiSong).toList();
+    }
+
+    @Override
+    public List<ConfetiArtist> getArtistsByIds(Collection<String> artistIds) {
+        String joinedArtistIds = String.join(QUERY_PARAMETER_IDS_DELIMITER, artistIds);
+        AppleMusicArtistsResponse fetchedArtistResponses = appleMusicFeignClient.getArtists(
+            joinedArtistIds);
+        return fetchedArtistResponses.toConfetiArtists();
     }
 }
--- src/main/java/confeti/confetibatchserver/external/service/MusicAPIHandler.java
+++ src/main/java/confeti/confetibatchserver/external/service/MusicAPIHandler.java
@@ -1,9 +1,13 @@
 package confeti.confetibatchserver.external.service;
 
+import confeti.confetibatchserver.domain.music.artist.vo.ConfetiArtist;
 import confeti.confetibatchserver.domain.music.song.vo.ConfetiSong;
+import java.util.Collection;
 import java.util.List;
 
 public interface MusicAPIHandler {
 
     List<ConfetiSong> getAllSongsByArtistId(String artistId);
+
+    List<ConfetiArtist> getArtistsByIds(Collection<String> artistIds);
 }
