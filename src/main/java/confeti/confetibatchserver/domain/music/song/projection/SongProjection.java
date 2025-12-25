40961dd7c28864a6ff703dbf9dc892a8bd016afa
jher235 <tim668666@gmail.com>
2025-12-25 23:43:34 +0900

    CONFETI-75 feat: projection을 사용해서 Song을 엔티티가 아닌 데이터로 조회하도록 변경 - DTO를 사용하는 경우 패키지 경로를 작성해야하므로 변경에 취약하다고 판단하여 변경. 패키지 경로가 잘못되어도 컴파일 오류로 못잡으므로..

---
+++ src/main/java/confeti/confetibatchserver/domain/music/song/projection/SongProjection.java
@@ -0,0 +1,14 @@
+package confeti.confetibatchserver.domain.music.song.projection;
+
+public interface SongProjection {
+
+    String getId();
+
+    String getTrackName();
+
+    String getArtworkUrl();
+
+    String getArtistName();
+
+    String getPreviewUrl();
+}
