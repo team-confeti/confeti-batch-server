89ed962bface8d4896a2dc6c35efed8290a48139
jher235 <tim668666@gmail.com>
2026-02-01 22:36:45 +0900

    CSERVER-5 refactor: 설정 클래스 분리 - 메인 클래스에서 해당 책임을 갖지 않도록 하기 위함 - 추후 테스트 코드 작성 시 불필요한 설정을 활성화하지 않기 위함

--- src/main/java/confeti/confetibatchserver/ConfetiBatchServerApplication.java
+++ src/main/java/confeti/confetibatchserver/ConfetiBatchServerApplication.java
@@ -2,11 +2,8 @@ package confeti.confetibatchserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
-@EnableFeignClients
+
 @SpringBootApplication
 public class ConfetiBatchServerApplication {
 
---
+++ src/main/java/confeti/confetibatchserver/global/config/FeignClientConfig.java
@@ -0,0 +1,10 @@
+package confeti.confetibatchserver.global.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients
+public class FeignClientConfig {
+
+}
