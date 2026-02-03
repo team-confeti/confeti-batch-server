aa5e165f2976dd2bac7b6ead6e3f589f16abca51
jher235 <tim668666@gmail.com>
2026-02-03 15:36:19 +0900

    CSERVER-5 feat: DB 내에 존재하지 않는 Artist Id만 반환하는 메서드 작성

--- src/main/java/confeti/confetibatchserver/domain/music/artist/infra/repository/ArtistRepository.java
+++ src/main/java/confeti/confetibatchserver/domain/music/artist/infra/repository/ArtistRepository.java
@@ -1,9 +1,15 @@
 package confeti.confetibatchserver.domain.music.artist.infra.repository;
 
 import confeti.confetibatchserver.domain.music.artist.Artist;
+import java.util.Collection;
+import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ArtistRepository extends JpaRepository<Artist, String>, ArtistJdbcRepository {
 
+    @Query("SELECT a.id FROM Artist a WHERE a.id IN :ids")
+    Set<String> findIdsIn(@Param("ids") Collection<String> ids);
 }
 
