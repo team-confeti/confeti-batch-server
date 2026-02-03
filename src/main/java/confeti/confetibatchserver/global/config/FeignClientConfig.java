5240cf52a6be0032a6d409315daa9b381c6926ba
jher235 <tim668666@gmail.com>
2026-02-03 13:12:58 +0900

    CSERVER-5 refactor: Client 클래스 명시 - @EnableFeignClients를 별도의 @Configuration으로 분리하며 FeignClient 인식을 못하여 변경함

--- src/main/java/confeti/confetibatchserver/global/config/FeignClientConfig.java
+++ src/main/java/confeti/confetibatchserver/global/config/FeignClientConfig.java
@@ -1,10 +1,11 @@
 package confeti.confetibatchserver.global.config;
 
+import confeti.confetibatchserver.external.client.AppleMusicFeignClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableFeignClients
+@EnableFeignClients(clients = {AppleMusicFeignClient.class})
 public class FeignClientConfig {
 
 }
