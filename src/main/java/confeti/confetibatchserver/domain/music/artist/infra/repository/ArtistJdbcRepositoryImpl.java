5d86b1ae8624905c55b9cd9149a1e497ddd86809
jher235 <tim668666@gmail.com>
2026-02-03 15:35:41 +0900

    CSERVER-5 refactor: 시간을 애플리케이션에서 주입하도록 변경

--- src/main/java/confeti/confetibatchserver/domain/music/artist/infra/repository/ArtistJdbcRepositoryImpl.java
+++ src/main/java/confeti/confetibatchserver/domain/music/artist/infra/repository/ArtistJdbcRepositoryImpl.java
@@ -2,6 +2,8 @@ package confeti.confetibatchserver.domain.music.artist.infra.repository;
 
 import confeti.confetibatchserver.domain.music.artist.Artist;
 import java.sql.PreparedStatement;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -11,18 +13,23 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class ArtistJdbcRepositoryImpl implements ArtistJdbcRepository {
 
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(
+        "yyyy-MM-dd HH:mm:ss");
     private final JdbcTemplate jdbcTemplate;
     private final String BULK_UPSERT_SQL = """
         INSERT INTO artists (id, name, artwork_url, created_at, updated_at)
-        VALUES (?, ?, ?, NOW(), null)
+        VALUES (?, ?, ?, ?, null)
         ON DUPLICATE KEY UPDATE
             name = VALUES(name),
             artwork_url = VALUES(artwork_url),
-            updated_at = NOW()
+            updated_at = VALUES(created_at)
         """;
 
     @Override
     public void upsertArtists(Collection<Artist> artists) {
+        LocalDateTime now = LocalDateTime.now();
+        String nowString = now.format(FORMATTER);
+
         jdbcTemplate.batchUpdate(BULK_UPSERT_SQL,
             artists,
             artists.size(),
@@ -30,6 +37,7 @@ public class ArtistJdbcRepositoryImpl implements ArtistJdbcRepository {
                 ps.setString(1, artist.getId());
                 ps.setString(2, artist.getName());
                 ps.setString(3, artist.getArtworkUrl());
+                ps.setObject(4, nowString);
             });
     }
 }
